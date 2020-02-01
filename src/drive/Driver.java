84daece8c4bef44a120197a74342577be66dd094
hailong1 <niumaozhan001@gmail.com>
2020-01-31 21:28:53 -0600

    VMmachine

---
+++ src/drive/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package drive;
+
+/**
+ * @author Hailong 
+ *
+ */
+public class Driver {
+
+
+	public static void main(String[] args) {
+		
+		new Display().begin();
+		
+          
+	}
+
+}
